2315acb8ca5b1c5c120e67ec1b77f390ba28e5d3
atanda rasheed <atandarash@gmail.com>
2021-11-01 01:17:38 +0100

    feat(trimmer): implement trimmer runner

--- lib/elasticlunr/pipeline/trimmer.ex
+++ lib/elasticlunr/pipeline/trimmer.ex
@@ -1,10 +1,15 @@
 defmodule Elasticlunr.Pipeline.Trimmer do
   @moduledoc false
 
+  alias Elasticlunr.Token
+
   @behaviour Elasticlunr.Pipeline
 
   @impl true
-  def call(token, _tokens) do
-    token
+  def call(%Token{token: str} = token, _tokens) do
+    str = Regex.replace(~r/^\W+/, str, "")
+    str = Regex.replace(~r/\W+$/, str, "")
+
+    Token.update(token, token: str)
   end
 end
