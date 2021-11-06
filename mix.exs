bdb084012698e90eb79ae233f9862251cfd8831a
atanda rasheed <atandarash@gmail.com>
2021-11-06 02:01:17 +0100

    ci: setup github action

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Elasticlunr.MixProject do
     [
       app: :elasticlunr,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
