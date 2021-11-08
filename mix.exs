095bae5713df5f75ad02157e7871e64c71c80cde
atanda rasheed <atandarash@gmail.com>
2021-11-08 23:05:50 +0100

    chore: release new version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Elasticlunr.MixProject do
   def project do
     [
       app: :elasticlunr,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
