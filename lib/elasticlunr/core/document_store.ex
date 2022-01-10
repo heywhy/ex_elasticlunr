16cc024fb178ca945bcd87897fe3a9facc631b02
atanda rasheed <atandarash@gmail.com>
2022-01-10 02:37:17 +0100

    chore: resolve warnings when generating docs

--- lib/elasticlunr/core/document_store.ex
+++ lib/elasticlunr/core/document_store.ex
@@ -1,5 +1,5 @@
 defmodule Elasticlunr.DocumentStore do
-  alias Elasticlunr.Index
+  alias Elasticlunr.{Field, Index}
 
   defstruct save: true, documents: %{}, document_info: %{}, length: 0
 
@@ -15,7 +15,7 @@ defmodule Elasticlunr.DocumentStore do
     struct!(%__MODULE__{}, %{save: save})
   end
 
-  @spec add(t(), Index.document_ref(), map()) :: t()
+  @spec add(t(), Field.document_ref(), map()) :: t()
   def add(%__MODULE__{documents: documents, length: length, save: save} = store, ref, document) do
     length =
       case exists?(store, ref) do
@@ -38,10 +38,10 @@ defmodule Elasticlunr.DocumentStore do
     %{store | length: length, documents: documents}
   end
 
-  @spec get(t(), Index.document_ref()) :: map() | nil
+  @spec get(t(), Field.document_ref()) :: map() | nil
   def get(%__MODULE__{documents: documents}, ref), do: Map.get(documents, ref)
 
-  @spec remove(t(), Index.document_ref()) :: map() | nil
+  @spec remove(t(), Field.document_ref()) :: map() | nil
   def remove(
         %__MODULE__{document_info: document_info, documents: documents, length: length} = store,
         ref
@@ -59,10 +59,10 @@ defmodule Elasticlunr.DocumentStore do
     end
   end
 
-  @spec exists?(t(), Index.document_ref()) :: boolean()
+  @spec exists?(t(), Field.document_ref()) :: boolean()
   def exists?(%__MODULE__{documents: documents}, ref), do: Map.has_key?(documents, ref)
 
-  @spec add_field_length(t(), Index.document_ref(), Index.document_field(), pos_integer()) :: t()
+  @spec add_field_length(t(), Field.document_ref(), Index.document_field(), pos_integer()) :: t()
   def add_field_length(%__MODULE__{document_info: document_info} = store, ref, field, length) do
     case exists?(store, ref) do
       false ->
@@ -79,12 +79,12 @@ defmodule Elasticlunr.DocumentStore do
     end
   end
 
-  @spec update_field_length(t(), Index.document_ref(), Index.document_field(), pos_integer()) ::
+  @spec update_field_length(t(), Field.document_ref(), Index.document_field(), pos_integer()) ::
           t()
   def update_field_length(%__MODULE__{} = store, ref, field, length),
     do: add_field_length(store, ref, field, length)
 
-  @spec get_field_length(t(), Index.document_ref(), Index.document_field()) :: pos_integer()
+  @spec get_field_length(t(), Field.document_ref(), Index.document_field()) :: pos_integer()
   def get_field_length(%__MODULE__{document_info: document_info} = store, ref, field) do
     case exists?(store, ref) do
       false ->
